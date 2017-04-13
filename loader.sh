868f1ad2f27c4f1a226c0ff221e81927239c83e5
ant <git@manchestermonkey.co.uk>
2017-04-13 11:01:30 +0000

    shellchecked everything
    A few issues still remain however

--- loader.sh
+++ loader.sh
@@ -1,15 +1,17 @@
+#!/bin/bash
+
 OURDIR=$(dirname "${BASH_SOURCE[0]}")
-for F in $OURDIR/lib/*-functions; do source $F; done
-
-
+for F in $OURDIR/lib/*-functions; do source "$F"; done
 
 ## Auto complete - configs
 complete -F _gcp-config-profile gcp
 complete -F _gcp-console gconsole
+
 ## Autocomplete - gci
 complete -F _gci-stop gci-stop
 complete -F _gci-start gci-start
 complete -F _gci-start gci-reset
+
 ## Autocomplete - gcd
 complete -F _gcd-zones gcd-zones
 complete -F _gcd-zones gcd
